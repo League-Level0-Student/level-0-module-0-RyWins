595a2b95996efe2ea2bb687d228506ccbdd08f7a
league <zanderzhao2020@gmail.com>
2022-07-27 16:58:09 -0700

    quiz game in progress

--- src/_03_print_and_popups/_2_madlibs/MadlibsOfTheAmazon.java
+++ src/_03_print_and_popups/_2_madlibs/MadlibsOfTheAmazon.java
@@ -9,26 +9,26 @@ public class MadlibsOfTheAmazon {
 		String prompt =  "If you find yourself having to cross a piranha-infested river, here's how to do it...";
 		
 		// Get the player to enter an adjective
-
+		String prompt1 = "viscious";
 		// Get the player to enter a type of liquid
-
+		String prompt2 = "The liquid is water";
 		// Get the player to enter a body part
-
+		String prompt3 = "toe";
 		// Get the player to enter a verb
-
+		String prompt4 = "Swim";
 		// Get the player to enter a place
-
+		String prompt5 = "Alaska";
 		// The story below has has been written as a group of Strings joined together by + signs.
 		// The story contains place holders, indicated by [** **] which you need to replace with
 		// the values entered by the player.
 		// Hint:  You will need to add more + signs to join the variables to the other parts of the story.
 		
 		String story = 
-		"Piranhas are more [**adjective**] during the day, so cross the river at\n"  +
-		"night. Piranhas are attracted to fresh [**type of liquid**] and will most\n"  +
-		"likely take a bite out of your [**body part**] if you [**verb**]. Whatever\n"  +
+		"Piranhas are more [**viscious**] during the day, so cross the river at\n"  +
+		"night. Piranhas are attracted to fresh [**water**] and will most\n"  +
+		"likely take a bite out of your [**toe**] if you [**swim**]. Whatever\n"  +
 		"you do, if you have an open wound, try to find another way to get\n"  +
-		"back to the [**place**]. Good luck!";
+		"back to the [**Alaska**]. Good luck!";
 		
 		// Make a pop-up that contains the final story. The \n escape characters add line breaks to the story. 
 		// If you need to, move them around to make your story look better in the pop-up
--- src/_03_print_and_popups/_4_quiz_game/QuizGame.java
+++ src/_03_print_and_popups/_4_quiz_game/QuizGame.java
@@ -1,18 +1,21 @@
 package _03_print_and_popups._4_quiz_game;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	public static void main(String[] args) {
 		
 		// Create a variable to hold the user's score. Set it equal to zero. 
-		
+		int Score = 0;
 		// ASK A QUESTION AND CHECK THE ANSWER
 		
 				// 2.  Ask the user a question 
-				
+				String prompt = JOptionPane.showInputDialog("How many states are in the U.S.");
 				// 3.  Use an if statement to check if their answer is correct
-				
+				Object input = null;
+				if(input.equals(50));{
 				// 4.  if the user's answer was correct, add one to their score 
-		
+					
 		// MAKE MORE QUESTIONS. Ask more questions by repeating the above 
 				// Option: Subtract a point from their score for a wrong answer
 		
@@ -21,3 +24,7 @@ public class QuizGame {
 		
 	}
 }
+
+	
+		
+	}
