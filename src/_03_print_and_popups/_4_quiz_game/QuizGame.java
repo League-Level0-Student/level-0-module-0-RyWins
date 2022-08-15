b15ad79d8d48023316c0a4fa33a55d907c65594f
league <league@imac-18.attlocal.net>
2022-08-15 15:47:32 -0700

    Starting Dragon Fight

--- src/_03_print_and_popups/_4_quiz_game/QuizGame.java
+++ src/_03_print_and_popups/_4_quiz_game/QuizGame.java
@@ -10,24 +10,37 @@ public class QuizGame {
 		// ASK A QUESTION AND CHECK THE ANSWER
 		
 				// 2.  Ask the user a question 
-				{
 				String answer = JOptionPane.showInputDialog("How many states are in the U.S.");
 				// 3.  Use an if statement to check if their answer is correct
 				
 				if(answer.equals("50"))
 				{
 				JOptionPane.showInputDialog("Correct");
+				
+				Score =+ 1;
 				}
+				else
+				
 				{
+					JOptionPane.showInputDialog("Incorrect");
+				}
+				
+				
 				String answer1 = JOptionPane.showInputDialog("What is 10 multiplied by 10 eqaul");
 				
-				if(answer1.equals("100"));
+				if(answer1.equals("100")) {
+					
+					JOptionPane.showInputDialog("Correct");
+					
+					int Score1 = + 1;
 				}
-				JOptionPane.showInputDialog("Correct");
-				}
+				else
+				
 				{
-				String input = JOptionPane.showInputDialog("Final score: 2");
+					JOptionPane.showInputDialog("Incorrect");
 				}
+				String input = JOptionPane.showInputDialog("Final score: 2");
+				
 				// 4.  if the user's answer was correct, add one to their score 
 					
 		// MAKE MORE QUESTIONS. Ask more questions by repeating the above 
@@ -37,7 +50,7 @@ public class QuizGame {
 		// After all the questions have been asked, tell the user their final score 
 		
 	
-}
+		}
 
 	
 		
--- src/_03_print_and_popups/_5_awesome_or_not/AwesomeOrNot.java
+++ src/_03_print_and_popups/_5_awesome_or_not/AwesomeOrNot.java
@@ -2,35 +2,52 @@ package _03_print_and_popups._5_awesome_or_not;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class AwesomeOrNot {
 
 	// 1. Make a main method that includes everything below
-
-	
-		Random ran = new Random(number);    //This will be used below to make a random number. 
+	public static void main(String[] args) { 
+		//Random ran = new Random();    //This will be used below to make a random number. 
 		
 		// 2. Make a variable that will hold a random whole number
-		int number = 
+		Random ran = new Random(); 
 		// 3. Set your variable equal to a positive number less than 4 using     ran.nextInt(4); 
 		ran.nextInt(4);
 		// 3. Print your variable to the console
-	
+		JOptionPane.showInputDialog(new Random());
 		// 4. Get the user to enter something that they think is awesome
-	
+		String answer = JOptionPane.showInputDialog("What is something that is awesome?");
 		// 5. If your variable is  0
+		
+		if(ran.equals(0));
+		{
+			JOptionPane.showInputDialog("Awesome");
+		}
 	
+		
+			
 			// -- tell the user whatever they entered is awesome!
 	
 		// 6. If your variable is  1
-	
+		if(ran.equals(1));
+		{
+			JOptionPane.showInputDialog("ok");	
+		}
 			// -- tell the user whatever they entered is ok.
-	
+		
 		// 7. If your variable is  2
-	
+		if(ran.equals(2));
+		{
+			JOptionPane.showInputDialog("Boring");	
+		}
 			// -- tell the user whatever they entered is boring.
 	
 		// 8. If your variable is  3
-	
+		if(ran.equals(3));
+		{
+			JOptionPane.showInputDialog("That is Awesome!");	
+		}
 			// -- invent your own message to give to the user (be nice).
-
+	}
 }
